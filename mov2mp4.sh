44fcb34812879c490df9bbafba38aae417c319d8
iatenothingbutriceforthreedays <darwinvickers@gmail.COM>
2022-10-28 13:41:23 -0400

    add a bunch more videos

--- mov2mp4.sh
+++ mov2mp4.sh
@@ -1,2 +1,20 @@
-# TODO this should be more automated
-ffmpeg -i $1 -vcodec h264 $2
+# Go through input files and convert all to mp4
+# delete originals (be careful!)
+
+# ffmpeg -i $1 -vcodec h264 $2
+
+for f in "$@"
+do
+  if [[ $f == *.MOV ]]
+  then
+    of="${f%.MOV}.mp4"
+    cmd="ffmpeg -y -i $f -vcodec h264 $of"
+    echo cmd
+    $cmd
+
+    cmd="rm $f"
+    rm $f
+    echo cmd
+    $cmd
+  fi
+done
