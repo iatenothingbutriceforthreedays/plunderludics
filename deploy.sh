4f0f3291d3252c3a78841f3b3e2dc5373ca0870d
iatenothingbutriceforthreedays <darwinvickers@gmail.COM>
2022-10-28 12:48:03 -0400

    better deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,23 @@
 # Checkout gh-pages branch
-git checkout gh-pages # TODO find a way around warnings for local files or whatever
+git checkout gh-pages && # TODO find a way around warnings for local files or whatever
 
 # Mirror main
-git reset --hard origin/main
+git reset --hard origin/main &&
 
 # Build files
-npm run build
+npm run build &&
 
 # Add and commit built files
-git add docs
-git commit -m "add built files"
+git add docs &&
+git commit -m "add built files" &&
 
 # Squash history (don't need versioning on generated files)
-git reset $(git commit-tree HEAD^{tree} -m "Erase history")
+git reset $(git commit-tree HEAD^{tree} -m "Erase history") &&
 
 # Force push
-git push -f
+git push -f &&
 
 # Back to main branch
-git checkout main
+git checkout main &&
+
+true;
