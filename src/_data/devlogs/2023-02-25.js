91e0d86d98141ab06eb784157a47dc90fee3ad96
mutmedia <gustavo.ceci95@gmail.com>
2023-02-26 15:26:35 -0500

    some documentation

---
+++ src/_data/devlogs/2023-02-25.js
@@ -0,0 +1,42 @@
+module.exports = {
+  name: "spring salad: a plunderludic gathering 2023-02-25",
+  date: "2023-02-25",
+  description: `we showed some of our work accompanied by some of the many references.
+
+bennett foddy wrote:
+"it was nice to see all the different things! I guess the main thing it makes me think about is the various different roles the emulated games play in these pieces - how it feels different in Patrick's '99 exercises' for it not to be emulated.
+
+In Jonny's tetris games, it is really important that it is an recognizable game, and I think also important that it is tetris in particular. Part of it is that it's reframing something that the player already knows how to do in a deep way as something that is hard or impossible... it sets up a metaphor for feeling overwhelmed in life, that there are tasks that would be easy if taken alone that become impossible taken as a set. Similarly in that Arcangel Tetris piece, it's important that everyone who sits down with it has an opinion about how it ought to be played - so there can be collaborative play, discussion and disagreement and so on. In these cases it's important that it looks like historic versions of tetris, and plays like tetris, but maybe it's not important that it's emulated?
+
+In your 'toggle death' mario game, it is important that it is emulated in particular, but not really important that the game is Mario or even that it's recognized by the player. It's playing with the idea of an attract mode, which is a fated loop that the character cannot escape, but you're breaking the rules and allowing him to escape death. 'Breaking the Rules' is an idea that only works because it's an emulated or hacked version of a 'real', released game. If you had access to the actual source code, it would be nothing.
+
+The game where you push on the walls to fade between platformers... I think in that case it's important that the games are 'real', but maybe less important that they are emulated. I think I said to you last night, the crossfade suggests the cinematic cliche of someone remembering something. So it feels like the player is thinking 'oh this is like that other time....'. I wish it wasn't walking into walls, but something that was more of a key moment in actual games the player had played? Because walking into walls is not really something memorable. But I loved the feeling of the crossfade.
+
+In multibowl I think it is important that it is running on an emulator, even though 99% of the games are unfamiliar to the players - so I'm not using the emulated games to establish familiarity or memory or literacy or anything like that. I've played games with a similar set of ideas that were all original (some NYU students including Ben Sironko made a game called 'Tuned Out' as their thesis which does this) and it still works, but i think there is a pretty big difference of feeling between 'I've made a new situation and put you in it' and 'I'm putting you in this situation that exists'.
+
+So, I haven't really thought it through but the ideas are something like
+- familiar games (emulated or unemulated) for literacy/nostalgia/conveying meaning
+- emulation being the arbiter of strict 'rules' or 'facts' about a game
+- emulated, familiar games being 'true memories'
+- emulated, unfamiliar games being 'true experiences'
+
+There are related ideas in other media. The one that comes to mind is Portishead's second album - I think having been burned by the cost of 'clearing' samples on their first record (which means, paying a licensing fee) they claimed - I have no idea if true or false, but sounds false - that they recorded a whole jazz record first, pressed it to vinyl, then used that as the source of samples and scratching for the final record. Although convenient for copyright reasons, I feel like collage constructed of work made by the collage-maker is missing something of the value in collage or any postmodernist form... it's 'hermetic', enclosed in itself, antisocial. One of the big reasons that sampling and collage and intertextualism are interesting and vital forms, is that there are multiple voices playing together."`,
+  assets: [
+  ],
+
+  experiments: [
+    "tapestry",
+    "mario-feedback",
+    "tonytony",
+  ],
+  "other-plunderludics": [
+    'tbaao',
+    'magic box',
+    '99 exercises in play',
+    'super slow tetris',
+    'auto mario',
+    'pacman quine',
+    'three-dimensional tetris display',
+    'multibowl'
+  ]
+}
